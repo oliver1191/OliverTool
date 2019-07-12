2511543d8d8c478968e448610abc604fddf7615a
Oliver.Lin <Oliver.Lin@avepoint.com>
2019-07-12 17:23:14 +0800

    AutoWriteGitIngoreFile project

---
+++ AutoWriteGitIngoreFile/GitIgnoreControll.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoWriteGitIngoreFile
+{
+    public class GitIgnoreControll
+    {
+        public void WriteIgnoreFile()
+        {
+            string folderPath = ConfigurationManager.AppSettings["foldPath"];
+            List<string> ignoreFolderNames = new List<string>();
+            NameValueCollection allSettings = ConfigurationManager.AppSettings;
+            foreach (var setting in allSettings)
+            {
+                if (setting.ToString() != "foldPath" && setting.ToString() != "logPath")
+                {
+                    ignoreFolderNames.Add(ConfigurationManager.AppSettings[setting.ToString()]);
+                }
+            }            
+            foreach (var folder in Directory.GetDirectories(folderPath))
+            {
+                string folderName = folder.Substring(folder.LastIndexOf("\\") + 1);                
+                if (folderName == ".git" || folderName == ".vs"||folderName== "packages")
+                {                    
+                    WriteStrtoFile(string.Format("/{0}", folderName));
+                }
+                else
+                {
+                    foreach (var item in ignoreFolderNames)
+                    {                                                
+                        WriteStrtoFile(string.Format("/{0}/{1}", folderName, item));
+                    }                    
+                }                                         
+            }
+        }
+        public void WriteStrtoFile(string message)
+        {           
+            string sFilePath = ConfigurationManager.AppSettings["logPath"];
+            string sFileName = DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+            sFileName = sFilePath + @"\\" + sFileName;
+            if (!Directory.Exists(sFilePath))
+            {
+                Directory.CreateDirectory(sFilePath);
+            }
+            FileStream fs;
+            StreamWriter sw;
+            if (System.IO.File.Exists(sFileName))
+            {
+                fs = new FileStream(sFileName, FileMode.Append, FileAccess.Write);
+            }
+            else
+            {
+                fs = new FileStream(sFileName, FileMode.Create, FileAccess.Write);
+            }
+            sw = new StreamWriter(fs);
+            sw.WriteLine(message);
+            sw.Close();
+            fs.Close();
+
+        }
+    }
+}
---
+++ AutoWriteGitIngoreFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace AutoWriteGitIngoreFile
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            GitIgnoreControll gitIgnore = new GitIgnoreControll();
+            gitIgnore.WriteIgnoreFile();
+            Console.WriteLine("success");
+            Console.ReadKey();
+        }
+    }
+}
